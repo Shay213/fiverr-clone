47e5285e06507f423fc809c0580e948a2b3352d6
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-05-06 19:32:37 +0200

    Add user and user options to navbar

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -4,6 +4,13 @@ import { useEffect, useState } from "react";
 
 export default function Navbar() {
   const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const currUser = {
+    id: 1,
+    username: "John Doe",
+    isSeller: true,
+  };
 
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
@@ -31,8 +38,30 @@ export default function Navbar() {
           <span>Explore</span>
           <span>English</span>
           <span>Sign in</span>
-          <span>Become a Seller</span>
-          <button>Join</button>
+          {!currUser?.isSeller && <span>Become a Seller</span>}
+          {!currUser && <button>Join</button>}
+          {currUser && (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <span>{currUser?.username}</span>
+              {open && (
+                <div className="options">
+                  {currUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add New Gig</span>
+                    </>
+                  )}
+                  <span>Orders</span>
+                  <span>Messages</span>
+                  <span>Logout</span>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
       {active && (
