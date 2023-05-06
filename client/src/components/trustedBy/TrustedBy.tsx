115a7d61f0c28be2a8bfc45bcbeb1aad706001b9
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-05-06 21:19:26 +0200

    Create and style trustedBy component

---
+++ client/src/components/trustedBy/TrustedBy.tsx
@@ -0,0 +1,16 @@
+import "./trustedBy.scss";
+
+export default function TrustedBy() {
+  return (
+    <div className="trustedBy">
+      <div className="container">
+        <span className="trustedText">Trusted by:</span>
+        <span>FACEBOOK</span>
+        <span>Google</span>
+        <span>NETFLIX</span>
+        <span>P&G</span>
+        <span>PayPal</span>
+      </div>
+    </div>
+  );
+}
