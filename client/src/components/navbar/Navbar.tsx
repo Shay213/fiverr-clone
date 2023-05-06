d3a82147f808f785aa28e2e718ba1b665ff93095
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-05-06 19:08:39 +0200

    Create navbar component and style it

---
+++ client/src/components/navbar/Navbar.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import "./navbar.scss";
+import { useState } from "react";
+
+export default function Navbar() {
+  const [active, setActive] = useState(false);
+  return (
+    <div className={active ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          <Link to="/">
+            <span className="text">fiverr</span>
+          </Link>
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          <span>Become a Seller</span>
+          <button>Join</button>
+        </div>
+      </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test</span>
+            <span>Test2</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const BASE_URL = import.meta.env.BASE_URL;
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter basename={BASE_URL}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
