1e6a76e6845004785d50daea7fbf3d4b6e46defc
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-05-08 16:49:59 +0200

    Create and style orders page

--- client/src/pages/orders/Orders.tsx
+++ client/src/pages/orders/Orders.tsx
@@ -1,5 +1,144 @@
 import "./orders.scss";
 
+const { BASE_URL } = import.meta.env;
+
 export default function Orders() {
-  return <div className="orders"></div>;
+  const currentUser = {
+    id: 1,
+    username: "John Doe",
+    isSeller: true,
+  };
+
+  return (
+    <div className="orders">
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>{currentUser?.isSeller ? "Buyer" : "Seller"}</th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/15182778/pexels-photo-15182778.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img
+                className="delete"
+                src={BASE_URL + "img/message.png"}
+                alt=""
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/15182778/pexels-photo-15182778.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img
+                className="delete"
+                src={BASE_URL + "img/message.png"}
+                alt=""
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/15182778/pexels-photo-15182778.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img
+                className="delete"
+                src={BASE_URL + "img/message.png"}
+                alt=""
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/15182778/pexels-photo-15182778.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img
+                className="delete"
+                src={BASE_URL + "img/message.png"}
+                alt=""
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/15182778/pexels-photo-15182778.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img
+                className="delete"
+                src={BASE_URL + "img/message.png"}
+                alt=""
+              />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/15182778/pexels-photo-15182778.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <img
+                className="delete"
+                src={BASE_URL + "img/message.png"}
+                alt=""
+              />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 }
