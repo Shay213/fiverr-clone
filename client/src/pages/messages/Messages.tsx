934ef1e0e2970e0b6a8f371c9bdc0f904d061f5d
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-05-08 17:07:40 +0200

    Create and style messages page

--- client/src/pages/messages/Messages.tsx
+++ client/src/pages/messages/Messages.tsx
@@ -1,5 +1,84 @@
+import { Link } from "react-router-dom";
 import "./messages.scss";
 
 export default function Messages() {
-  return <div className="messages"></div>;
+  const message =
+    "Lorem ipsum dolor, sit amet consectetur adipisicing.elit.consectetur adipisicing elit.lolor, sit amet consectetur adipisicing.elit.consectetur adipisicing elit";
+  return (
+    <div className="messages">
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Buyer</th>
+            <th>Last Message</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+          <tr className="active">
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 hour ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr className="active">
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 hour ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 hour ago</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 hour ago</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 hour ago</td>
+          </tr>
+          <tr>
+            <td>John Doe</td>
+            <td>
+              <Link to="/message/123" className="link">
+                {message.substring(0, 100)}...
+              </Link>
+            </td>
+            <td>1 hour ago</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 }
