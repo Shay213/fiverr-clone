7984ac15d110cb01f794bd56719589ec97e4cc2a
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-05-13 11:07:05 +0200

    Create conversations

--- server/src/controllers/conversation.controller.ts
+++ server/src/controllers/conversation.controller.ts
@@ -1,5 +1,53 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 
-export const deleteUser = async (req: FastifyRequest, reply: FastifyReply) => {
-  reply.send("from controller");
+interface Payload {
+  userId: string;
+  isSeller: boolean;
+}
+
+interface GetConversationsBody {
+  to: string;
+}
+
+export const createConversation = async (
+  req: FastifyRequest,
+  reply: FastifyReply
+) => {
+  const { sendError } = req.server.replyHelpers;
+  const { isSeller, userId } = req.payload as Payload;
+  const { to } = req.body as GetConversationsBody;
+
+  const conversationId = isSeller ? userId + to : to + userId;
+  const sellerId = isSeller ? userId : to;
+  const buyerId = isSeller ? to : userId;
+
+  try {
+    const newConversation = await req.server.prisma.conversation.create({
+      data: {
+        id: conversationId,
+        seller: { connect: { id: sellerId } },
+        buyer: { connect: { id: buyerId } },
+        readBySeller: isSeller,
+        readByBuyer: !isSeller,
+      },
+    });
+    return reply.code(201).send(newConversation);
+  } catch (error: any) {
+    return sendError(reply, error.message, 500);
+  }
 };
+
+export const getConversations = async (
+  req: FastifyRequest,
+  reply: FastifyReply
+) => {};
+
+export const getSingleConversation = async (
+  req: FastifyRequest,
+  reply: FastifyReply
+) => {};
+
+export const updateConversation = async (
+  req: FastifyRequest,
+  reply: FastifyReply
+) => {};
--- server/src/routes/conversation.route.ts
+++ server/src/routes/conversation.route.ts
@@ -1,9 +1,57 @@
 import { FastifyInstance } from "fastify";
+import {
+  getConversations,
+  createConversation,
+  getSingleConversation,
+  updateConversation,
+} from "../controllers/conversation.controller.ts";
+
+const CreateConversationSchema = {
+  body: {
+    type: "object",
+    properties: {
+      to: { type: "string" },
+    },
+    required: ["to"],
+  },
+  response: {
+    201: {
+      type: "object",
+      properties: {
+        id: { type: "string" },
+        sellerId: { type: "string" },
+        buyerId: { type: "string" },
+        readBySeller: { type: "boolean" },
+        readByBuyer: { type: "boolean" },
+        lastMessage: { type: ["string", "null"], nullable: true },
+        createdAt: { type: "string", format: "date-time" },
+        updatedAt: { type: "string", format: "date-time" },
+      },
+    },
+  },
+};
 
 export default function conversationRoute(
   fastify: FastifyInstance,
   options: object,
   done: () => void
 ) {
+  fastify.post(
+    "/",
+    { onRequest: [fastify.authenticate], schema: CreateConversationSchema },
+    createConversation
+  );
+  fastify.get("/", { onRequest: [fastify.authenticate] }, getConversations);
+
+  fastify.get(
+    "/single/:id",
+    { onRequest: [fastify.authenticate] },
+    getSingleConversation
+  );
+  fastify.post(
+    "/:id",
+    { onRequest: [fastify.authenticate] },
+    updateConversation
+  );
   done();
 }
